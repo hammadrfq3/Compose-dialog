1b318bb83082871555d0bd0bc9feea4a907dca0a
Hammad Rafiq <hammadrfq3@gmail.com>
2024-08-01 16:56:01 +0500

    jmklfdczsjkimhcfdzxjkhnczncjkcxjcxn

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.5.1")
-    }
-}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
